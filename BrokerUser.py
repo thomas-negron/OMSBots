3da6b08f7835eeb44a613f615916a048a8a1e981
thomas-negron <97264018+thomas-negron@users.noreply.github.com>
2022-04-13 14:55:34 -0400

    Update BrokerUser.py

--- BrokerUser.py
+++ BrokerUser.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 import time
 from settings import CLIENT_ID, LOGIN_ID, STRING
 import pandas as pd
-import geckodriver_autoinstaller
 
 
 def addBroker(entity, city, state, zip):
@@ -131,7 +130,6 @@ def addPerson(fname, lname, email, city, state, zip, recordType):
 
 
 # Set driver to use Firefox
-geckodriver_autoinstaller.install();
 myDriver = webdriver.Firefox()
 myDriver.maximize_window()
 
