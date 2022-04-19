a1f4beb4a53ed06e8072fa6b4196755368c93985
thomas-negron <97264018+thomas-negron@users.noreply.github.com>
2022-04-19 17:10:58 -0400

    Update BrokerUser.py

--- BrokerUser.py
+++ BrokerUser.py
@@ -133,11 +133,6 @@ def addPerson(fname, lname, email, city, state, zip, recordType):
 # Set driver to use Firefox
 #driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 #myDriver = webdriver.Firefox(executable_path="/home/OMSBots/geckodriver.exe")
-options = Options()
-options.headless = True
-myDriver = webdriver.Firefox(options=options)
-
-firefox_options = webdriver.FirefoxOptions()
 myDriver = webdriver.Remote(
     command_executor='http://10.116.0.2:4444',
     options=firefox_options
